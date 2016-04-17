fbf110871fef41926f8a86234af7bf9a63a22dc4
Ning, Tao <ningtaous@gmail.com>
2016-04-17 18:11:03 -0400

    refactor:  - separate particles for client  - add client homepage and blogs to handlebar layout

--- routes/client.js
+++ routes/client.js
@@ -7,128 +7,18 @@ var randomInt = function() {
   return Math.floor(Math.random() * 6 + 1);
 }
 
-router.get('/homepage.html', function(req, res) {
-  res.sendFile(path.join(__dirname, '../views/homepage.html'));
+router.get('/homepage*', function(req, res) {
+  res.render('partials/client/homepage', { layout: 'client' });
 });
 
-router.get('/blogs.html', function(req, res) {
-  res.sendFile(path.join(__dirname, '../views/blogs.html'));
-});
-
-router.get('/test', function(req, res) {
-  Article.find({}, null, {sort: '-date'}, function(err, docs) {
+router.get('/blogs*', function(req, res) {
+  Article.find({}, null, { sort: '-date' }, function(err, docs) {
     docs.forEach(function(item, index, array) {
       console.log(item);
       item.imgNum = randomInt();
     });
-    res.render('blogs', { articles: docs });
+    res.render('partials/client/blogs', { layout: 'client', articles: docs });
   });
-});
-
-var dataObj = {
-  articles: [
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    },
-    {
-      title: 'This is a Standard post with a Preview Image',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est' +
-      ' tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut ' +
-      'perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae' +
-      'voluptatem libero at eveniet veritatis ab facere.',
-      imgNum: randomInt()
-    }
-  ]
-
-
-}
-router.get('/blogs', function(req, res) {
-  res.render('blogs', dataObj);
 });
 
 module.exports = router;
