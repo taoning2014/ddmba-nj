2f3610641b721f8263f9557149324f6f667ada5b
Ning, Tao <ningtaous@gmail.com>
2016-04-18 12:21:50 -0400

    feat: add logout

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,13 @@ var Strategy = require('passport-local').Strategy;
 
 var Article = require('../mongodb/models').Article;
 
-router.use(passport.initialize());
-router.use(passport.session());
-
+function isUserLogin(req, res, next) {
+  if (!req.user) {
+    next();
+  } else {
+    res.redirect('/admin/login', { message: 'User not login' });
+  }
+}
 // High level serialize/de-serialize configuration for passport
 passport.serializeUser(function(user, done) {
 //  console.log('in serializeUser, put user data into session storage');
@@ -39,12 +43,31 @@ passport.use(new Strategy(
     return cb(null, user);
   }));
 
+router.use(passport.initialize());
+router.use(passport.session());
+
 router.get('/login', function(req, res) {
   res.render('partials/admin/login');
 });
 
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: '/fail' }),
+  function(req, res) {
+    res.redirect('/admin/selectAction');
+  });
+
 router.get('/fail', function(req, res) {
   res.render('partials/admin/fail');
+});
+
+// test if user is login
+// TODO, will rewrite as express middleware
+router.get('/*', function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/admin/login');
+  }
 });
 
 router.get('/selectAction', function(req, res) {
@@ -80,16 +103,10 @@ router.get('/editArticlePage/:_id', function(req, res) {
   });
 });
 
-router.post('/login',
-  passport.authenticate('local', { failureRedirect: '/fail' }),
-  function(req, res) {
-    res.redirect('/admin/selectAction');
-  });
-
 router.get('/logout', function(req, res) {
   console.log('hit logout');
   req.logout();
-  res.send('good');
+  res.redirect('/admin/login');
 });
 
 module.exports = router;
